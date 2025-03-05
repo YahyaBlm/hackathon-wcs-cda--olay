1fdcfe5e7ff54dcf2778f59c93e2a70de1ad43c1
YahyaBlm <yahya.belmou@gmail.com>
2025-03-05 16:17:02 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import Footer from './components/footer';
 import Header from './components/header';
+import './main.css';
 
 function App() {
     return (
