72807268cea9bc7c48cb98b71d27e579ca88263a
sadjoaldi <alsannealdi@hotmail.com>
2025-03-06 14:44:07 +0100

    display details

--- src/pages/discovered-desert.jsx
+++ src/pages/discovered-desert.jsx
@@ -1,15 +1,45 @@
 /* eslint-disable no-unused-vars */
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 import { Outlet } from 'react-router-dom';
-import { destinationsList } from '../data/destinations-list';
-
 import Img from '../assets/images/forest.jpg';
 import DiscoveredCard from '../components/discovered-card';
 import DiscoveredFooter from '../components/discovered-footer';
 import DiscoveredHeader from '../components/discovered-header';
+import { destinationsList } from '../data/destinations-list';
+
+// Données des déserts
+const desertData = [
+    {
+        name: 'desert1',
+        imgSrc: '../assets/images/home-sky.jpg',
+        location: 'clime',
+        text: 'soth',
+    },
+    {
+        name: 'desert2',
+        imgSrc: 'path/to/image2.jpg',
+        location: 'Jerry',
+        text: 'mlqsdjfpoi',
+    },
+    {
+        name: 'desert3',
+        imgSrc: 'path/to/image3.jpg',
+        location: 'location3',
+        text: 'text3',
+    },
+];
 
 export default function DiscoveredDesert() {
     const desertDestinations = destinationsList.filter((dest) => dest.name.startsWith('desert'));
+
+    const [selectedDestinationIndex, setSelectedDestinationIndex] = useState(null);
+
+    const handleCardClick = (index) => {
+        console.log('got it');
+        setSelectedDestinationIndex(index);
+    };
+
     return (
         <>
             <DiscoveredHeader className={'desert-background'} />
@@ -32,10 +62,24 @@ export default function DiscoveredDesert() {
                                 img={Img}
                                 city={destination.location}
                                 description={destination.description}
+                                onClick={() => handleCardClick(index)}
                             />
                         ))}
                         <Outlet />
                     </div>
+
+                    {selectedDestinationIndex !== null && (
+                        <div className="destination-info" id="info">
+                            <img
+                                src={desertData[selectedDestinationIndex].imgSrc}
+                                alt={desertData[selectedDestinationIndex].name}
+                            />
+                            <div className="destination-text">
+                                <h3>{desertData[selectedDestinationIndex].name}</h3>
+                                <p>{desertData[selectedDestinationIndex].text}</p>
+                            </div>
+                        </div>
+                    )}
                 </motion.div>
             </main>
             <DiscoveredFooter className={'desert-background'} />
--- src/pages/discovered-forest.jsx
+++ src/pages/discovered-forest.jsx
@@ -3,14 +3,44 @@ import { motion } from 'framer-motion';
 import { Outlet } from 'react-router-dom';
 import { destinationsList } from '../data/destinations-list';
 
+import { useState } from 'react';
 import Img from '../assets/images/forest.jpg';
 import DiscoveredCard from '../components/discovered-card';
 import DiscoveredFooter from '../components/discovered-footer';
 import DiscoveredHeader from '../components/discovered-header';
 import '../css/discovered-forest.css';
 
+const forestData = [
+    {
+        name: 'forest1',
+        imgSrc: '',
+        location: '',
+        text: 'formei,t',
+    },
+    {
+        name: 'forest2',
+        imgSrc: '',
+        location: 'forest 2',
+        text: 'mlqsdjfpoi',
+    },
+    {
+        name: 'forest3',
+        imgSrc: 'gohglle',
+        location: 'location3',
+        text: 'text3',
+    },
+];
+
 export default function DiscoveredForest() {
     const forestDestinations = destinationsList.filter((dest) => dest.name.startsWith('forest'));
+
+    const [selectedDestinationIndex, setSelectedDestinationIndex] = useState(null);
+
+    const handleCardClick = (index) => {
+        console.log('got it');
+        setSelectedDestinationIndex(index);
+    };
+
     return (
         <>
             <DiscoveredHeader className={'forest-background'} />
@@ -32,10 +62,24 @@ export default function DiscoveredForest() {
                             img={Img}
                             city={destination.location}
                             description={destination.description}
+                            onClick={() => handleCardClick(index)}
                         />
                     ))}
                     <Outlet />
                 </div>
+
+                {selectedDestinationIndex !== null && (
+                    <div className="destination-info">
+                        <img
+                            src={forestData[selectedDestinationIndex].imgSrc}
+                            alt={forestData[selectedDestinationIndex].name}
+                        />
+                        <div className="destination-text">
+                            <h3>{forestData[selectedDestinationIndex].name}</h3>
+                            <p>{forestData[selectedDestinationIndex].text}</p>
+                        </div>
+                    </div>
+                )}
             </motion.main>
             <DiscoveredFooter className={'forest-background'} />
         </>
