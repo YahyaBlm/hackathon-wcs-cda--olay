14f379c8958c0226898f649fb3db486cee495683
YahyaBlm <yahya.belmou@gmail.com>
2025-03-06 12:24:15 +0100

    fix bugs

--- src/pages/discovered-sea.jsx
+++ src/pages/discovered-sea.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import { destinationsList } from '../data/destinations-list';
 
-import '../css/discovered-sea.css';
 import DiscoveredFooter from '../components/discovered-footer';
 import DiscoveredHeader from '../components/discovered-header';
 import DiscoveredCard from '../components/discovered-card';
